f2335cd7a4ce7a288ee9f43aaa8eb2bb97a2c7a8
Octav <octav@octav.name>
2021-09-07 16:28:50 +0300

    Use AvailabilityChecker to check if product is in stock

--- src/EventSubscriber/NotifierSubscriber.php
+++ src/EventSubscriber/NotifierSubscriber.php
@@ -6,6 +6,7 @@ namespace Tavy315\SyliusProductSubscriptionsPlugin\EventSubscriber;
 
 use Sylius\Bundle\ResourceBundle\Event\ResourceControllerEvent;
 use Sylius\Component\Core\Model\ProductVariant;
+use Sylius\Component\Inventory\Checker\AvailabilityCheckerInterface;
 use Sylius\Component\Product\Model\Product;
 use Sylius\Component\Product\Model\ProductInterface;
 use Tavy315\SyliusProductSubscriptionsPlugin\Entity\SubscriptionInterface;
@@ -14,14 +15,20 @@ use Tavy315\SyliusProductSubscriptionsPlugin\Service\SubscriptionNotifier;
 
 final class NotifierSubscriber
 {
+    private AvailabilityCheckerInterface $availabilityChecker;
+
     private SubscriptionNotifier $subscriptionNotifier;
 
-    private SubscriptionRepositoryInterface $repository;
+    private SubscriptionRepositoryInterface $subscriptionRepository;
 
-    public function __construct(SubscriptionRepositoryInterface $repository, SubscriptionNotifier $subscriptionNotifier)
-    {
-        $this->repository = $repository;
+    public function __construct(
+        AvailabilityCheckerInterface    $availabilityChecker,
+        SubscriptionNotifier            $subscriptionNotifier,
+        SubscriptionRepositoryInterface $subscriptionRepository
+    ) {
+        $this->availabilityChecker = $availabilityChecker;
         $this->subscriptionNotifier = $subscriptionNotifier;
+        $this->subscriptionRepository = $subscriptionRepository;
     }
 
     public function productNotification(ResourceControllerEvent $event): void
@@ -29,11 +36,21 @@ final class NotifierSubscriber
         /** @var Product $product */
         $product = $event->getSubject();
 
+        if (!$product->isEnabled()) {
+            return;
+        }
+
         if ($product->getEnabledVariants()->count() === 0) {
             return;
         }
 
-        if (!$product->getEnabledVariants()->first()->isInStock()) {
+        $productVariant = $product->getEnabledVariants()->first();
+
+        if (!$productVariant->isEnabled()) {
+            return;
+        }
+
+        if (!$this->availabilityChecker->isStockAvailable($productVariant)) {
             return;
         }
 
@@ -45,16 +62,22 @@ final class NotifierSubscriber
         /** @var ProductVariant $productVariant */
         $productVariant = $event->getSubject();
 
-        if (!$productVariant->isInStock()) {
+        if (!$this->availabilityChecker->isStockAvailable($productVariant)) {
             return;
         }
 
-        $this->sendProductNotification($productVariant->getProduct());
+        $product = $productVariant->getProduct();
+
+        if (!$product || !$product->isEnabled()) {
+            return;
+        }
+
+        $this->sendProductNotification($product);
     }
 
     private function sendProductNotification(ProductInterface $product): void
     {
-        $subscriptions = $this->repository->findBy([
+        $subscriptions = $this->subscriptionRepository->findBy([
             'product' => $product,
             'status'  => SubscriptionInterface::STATUS_NEW,
         ]);
@@ -64,7 +87,7 @@ final class NotifierSubscriber
 
             $subscription->setStatus(SubscriptionInterface::STATUS_SENT);
 
-            $this->repository->add($subscription);
+            $this->subscriptionRepository->add($subscription);
         }
     }
 }
